a910ed468ba02e582b78544e2b9b7dd5b4b4350e
Daniel Shimield <dxshim90@gmail.com>
2019-02-19 22:31:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
-  def name = (new_name)
+  def name=(new_name)
     @name = new_name
   end
   def name
     @name
   end
-  def job = (new_job)
+  def job=(new_job)
     @job = new_job
   end
   def job 
